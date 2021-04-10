e02e377fd22274b917fed535c1415491f7ee02af
Tyler Pomberg <tyler642101@tamu.edu>
2021-04-10 10:44:28 -0500

    added back mysite with gettingstarted

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,22 +1,21 @@
-"""mysite URL Configuration
+from django.urls import path, include
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import include, path
+
+admin.autodiscover()
+
+import hello.views
+
+# To add a new path, first import the app:
+# import blog
+#
+# Then add the new path:
+# path('blog/', blog.urls, name="blog")
+#
+# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path("", hello.views.index, name="index"),
+    path("db/", hello.views.db, name="db"),
+    path("admin/", admin.site.urls),
 ]
