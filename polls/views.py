403d3ade0f468c6882bc6bf35f72b2c3ea5f27d6
Tyler Pomberg <tyler642101@tamu.edu>
2021-04-11 16:03:51 -0500

    update room.html

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
+import requests
 
 # Create your views here.
 def index(request):
@@ -16,6 +17,23 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+    '''
+    URL = "https://www.googleapis.com/youtube/v3/search"
+    key = "AIzaSyBtQEH431JmblFH_MAJpLC9AoyGCCOYC54"
+    param1 = "snippet"
+    param2 = "cats"
+    param3 = "1"
+    param4 = "relavance"
+    param5 = "video"
+    param6 = "true"
+    PARAMS = {'key':key, 'part':param1, 'q':param2, 'maxResults':param3, 'order':param4, 'type':param5, 'videoEmbeddable':param6}
+    r = requests.get(url=URL, params=PARAMS)
+    data = r.json()
+    
+    for record in data['items']:
+        list1 = record['id']['videoId']
+    return HttpResponse(list1)
+    '''
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
